c12d8ed6714b962ab7cbff1fc833009d3c954db7
Marcelo <miuccimarcelo@gmail.com>
2022-09-06 17:01:11 -0300

    Projeto

---
+++ src/main/java/tech/ada/oitomeiaquatro/excecoes/AcabouASenhaExcecao.java
@@ -0,0 +1,8 @@
+package tech.ada.oitomeiaquatro.excecoes;
+
+public class AcabouASenhaExcecao extends Exception {
+    public AcabouASenhaExcecao(){
+        super("Acabaram as Senhas nesta data");
+    }
+
+}
